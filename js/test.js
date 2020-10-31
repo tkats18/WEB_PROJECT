bd415f01698411cd5a4008c67c7c03229410480a
tkats18 <tkats18@freeuni.edu.ge>
2020-11-01 02:17:41 +0400

    startung with the main page..

--- js/test.js
+++ js/test.js
@@ -1,5 +1,10 @@
 
 
-function getFood(){
-    
+function makeInvisible(id){
+    document.getElementById(id).style.visibility='hidden'
+}
+
+function makeVisible(id){
+    document.getElementById(id).style.visibility=''
+
 }

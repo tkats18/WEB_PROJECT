787b4199352d4f765380851ac5629c86d9e8a539
tkats18 <tkats18@freeuni.edu.ge>
2020-11-03 02:46:13 +0400

    fixed the menu item centrying issue

--- js/test.js
+++ js/test.js
@@ -1,10 +1,13 @@
 
 
 function makeInvisible(id){
-    document.getElementById(id).style.visibility='hidden'
+    document.getElementById(id).style.display='none'
+    
 }
 
 function makeVisible(id){
-    document.getElementById(id).style.visibility=''
-
+    document.getElementById(id).style.display='block'
+    document.getElementById(id).style.left = 
+        document.getElementById(id+"_par").getBoundingClientRect().left+ document.getElementById(id+"_par").getBoundingClientRect().width/2
+        -document.getElementById(id).getBoundingClientRect().width/2
 }
